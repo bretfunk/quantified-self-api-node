fa69e56e92abe1178081957fb5ff3e49aed4cd3f
BFunk <bret.e.funk@gmail.com>
2017-10-18 11:41:27 -0600

    working food get, post and delete methods

--- server.js
+++ server.js
@@ -1,26 +1,57 @@
 let express = require('express')
 let app = express()
-let bodyParser = require('body-parser')
+  let bodyParser = require('body-parser')
+  const environment = process.env.NODE_ENV || 'development'
+  const configuration = require('./knexfile')[environment]
+  const database = require('knex')(configuration)
 
-app.use(bodyParser.json()) //parses JSON
+  app.use(bodyParser.json()) //parses JSON
 app.use(bodyParser.urlencoded({ extended: true })) //parses HTML forms
 
-app.set('port', process.env.PORT || 3000)
-app.locals.title = 'Quantified Self'
+  app.set('port', process.env.PORT || 3000)
+  app.locals.title = 'Quantified Self'
 
-app.get('/api/v1/foods', function(request, response) {
-  response.send('all foods currently in database')
-    //response.json({'name': 'Bret'}
-})
+//app.get('/', function(request, response) {
+  //database('meals').innerJoin('meal_foods', 'meals.id', 'meal_foods.meal_id')
+    //.innerJoin('foods', 'foods.id', 'meal_foods.food_id')
+    //.select([
+        //'meals.id AS mealId',
+        //'meals.name AS mealName',
+        //database.raw('ARRAY_AGG(foods.name)'
+     //)])
+    //.groupBy('meals.id', 'meals.name')
+      //.then((data) => {
+      //response.json(data.rows)
+        //process.exit()
+        //})
+//})
+
+  app.get('/api/v1/foods', function(request, response) {
+    database.raw('SELECT * FROM foods ORDER BY id')
+      .then((data) => {
+        response.json(data.rows)
+          process.exit()
+      })
+  })
 
 app.get('/api/v1/foods/:id', function(request, response) {
-  response.send('returns foods with that id, 404 if not found')
-    //response.json({'name': 'Bret'}
+  database.raw('SELECT * FROM foods WHERE id=?', [request.params.id])
+    .then((data) => {
+      response.json(data.rows)
+        process.exit()
+    })
 })
 
 app.post('/api/v1/foods', function(request, response) {
-  response.json({})
-    //needs 400 status code if unsucessful
+  database.raw('INSERT INTO foods (name, calories, created_at, updated_at) VALUES (?, ?, ?, ?)',
+      [request.param('name'), request.param('calories'), new Date, new Date])
+    .then(() => {
+      return database.raw('SELECT * FROM foods ORDER BY id')
+    })
+  .then((data) => {
+    response.json(data.rows)
+    process.exit()
+  })
 })
 
 app.patch('/api/v1/foods/:id', function(request, response) {
@@ -29,7 +60,14 @@ app.patch('/api/v1/foods/:id', function(request, response) {
 })
 
 app.delete('/api/v1/foods/:id', function(request, response) {
-  //if food not found return a 404
+  database.raw('DELETE FROM foods WHERE id=?', [request.params.id])
+    .then(() => {
+      return database.raw('SELECT * FROM foods ORDER BY id')
+    })
+  .then((data) => {
+    response.json(data.rows)
+    process.exit()
+  })
 })
 
 
