d93ba5537f3d2324e2b40dc97bd4ad73f789434a
BFunk <bret.e.funk@gmail.com>
2017-10-18 15:09:16 -0600

    working on patch, having issues

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true })) //parses HTML forms
   app.set('port', process.env.PORT || 3000)
   app.locals.title = 'Quantified Self'
 
-//app.get('/', function(request, response) {
+//app.get('/', function(req, res) {
   //database('meals').innerJoin('meal_foods', 'meals.id', 'meal_foods.meal_id')
     //.innerJoin('foods', 'foods.id', 'meal_foods.food_id')
     //.select([
@@ -21,51 +21,60 @@ app.use(bodyParser.urlencoded({ extended: true })) //parses HTML forms
      //)])
     //.groupBy('meals.id', 'meals.name')
       //.then((data) => {
-      //response.json(data.rows)
+      //res.json(data.rows)
         //process.exit()
         //})
 //})
 
-  app.get('/api/v1/foods', function(request, response) {
+  app.get('/api/v1/foods', function(req, res) {
     database.raw('SELECT * FROM foods ORDER BY id')
       .then((data) => {
-        response.json(data.rows)
+        res.json(data.rows)
           process.exit()
       })
   })
 
-app.get('/api/v1/foods/:id', function(request, response) {
-  database.raw('SELECT * FROM foods WHERE id=?', [request.params.id])
+app.get('/api/v1/foods/:id', function(req, res) {
+  database.raw('SELECT * FROM foods WHERE id=?', [req.params.id])
     .then((data) => {
-      response.json(data.rows)
+      res.json(data.rows)
         process.exit()
     })
 })
 
-app.post('/api/v1/foods', function(request, response) {
+app.post('/api/v1/foods', function(req, res) {
   database.raw('INSERT INTO foods (name, calories, created_at, updated_at) VALUES (?, ?, ?, ?)',
-      [request.param('name'), request.param('calories'), new Date, new Date])
+      [req.param('name'), req.param('calories'), new Date, new Date])
     .then(() => {
       return database.raw('SELECT * FROM foods ORDER BY id')
     })
   .then((data) => {
-    response.json(data.rows)
+    res.json(data.rows)
     process.exit()
   })
 })
 
-app.patch('/api/v1/foods/:id', function(request, response) {
+app.patch('/api/v1/foods/:id', function(req, res) {
+  debugger
+    let name = req.query.name
+    let calories = req.query.calories
+    let id = req.params.id
+    console.log(req.query.name)
+    console.log(req.query.calories)
+    console.log(req.params.id)
+
+    //database.raw('UPDATE foods SET ? = ?, ? = ? WHERE id = ?')
   //{ food: { name: "Name of Food", calories: "Calories here" } }
   //if not sucessful a 300 status code will be returned
 })
 
-app.delete('/api/v1/foods/:id', function(request, response) {
-  database.raw('DELETE FROM foods WHERE id=?', [request.params.id])
+app.delete('/api/v1/foods/:id', function(req, res) {
+  database.raw('DELETE FROM foods WHERE id=?', [req.params.id])
     .then(() => {
       return database.raw('SELECT * FROM foods ORDER BY id')
     })
   .then((data) => {
-    response.json(data.rows)
+    res.json(data.rows)
     process.exit()
   })
 })
