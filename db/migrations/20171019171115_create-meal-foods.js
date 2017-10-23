28add0f62e612828bdff3a6e73665342308fa2e1
BFunk <bret.e.funk@gmail.com>
2017-10-23 14:18:15 -0600

    heroku updates

--- db/migrations/20171019171115_create-meal-foods.js
+++ db/migrations/20171019171115_create-meal-foods.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   let createMealFoodsTable = `CREATE TABLE meal_foods(
     id SERIAL PRIMARY KEY NOT NULL,
-    meal_id INTEGER REFERENCES meals(id),
-    food_id INTEGER REFERENCES foods(id),
+    meal_id INTEGER,
+    food_id INTEGER,
     created_at TIMESTAMP,
     updated_at TIMESTAMP
   )`
